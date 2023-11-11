f42a07293b02fa2023caf599a56f04337e16bc82
Martin <escorpmartin97@gmail.com>
2023-11-11 16:36:18 -0300

    feat: validation of temperature

--- src/user_inputs/validations.py
+++ src/user_inputs/validations.py
@@ -1,8 +1,11 @@
 def validate_month(month):
   if not(month >= 1 and month <= 12):
     raise ValueError("O valor do mês é inválido. Insira um número entre 1 e 12.")
-  else:
-    return month
+  
+  return month
 
 def validate_temperature(temperature):
- return temperature
+  if not(temperature >= 60 and temperature <= 50):
+    raise ValueError("A temperatura tem que estar dentro de um intervalo de -60 graus à +50 graus.")
+    
+  return temperature
