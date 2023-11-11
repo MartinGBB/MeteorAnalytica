001fba655c2a2a5a3678be26fbc93329000ca67f
Martin <escorpmartin97@gmail.com>
2023-11-11 16:30:58 -0300

    refator: Change the parameter name for the month validation

--- src/user_inputs/validations.py
+++ src/user_inputs/validations.py
@@ -1,8 +1,8 @@
-def validate_month(value):
-  if not(value >= 1 and value <= 12):
+def validate_month(month):
+  if not(month >= 1 and month <= 12):
     raise ValueError("O valor do mês é inválido. Insira um número entre 1 e 12.")
   else:
-    return value
+    return month
 
-def validate_temperature(value):
-  return value
+def validate_temperature(temperature):
+ return temperature
